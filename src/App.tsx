bbf0ffc3264a7d72934c49ae5b24f0afb269cf1f
eshux <54838488+eshux@users.noreply.github.com>
2020-12-23 16:27:30 +0200

    Blog

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import './App.css';
+import 'flexboxgrid';
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
