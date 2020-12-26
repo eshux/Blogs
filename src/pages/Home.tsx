68571373ff5b540289855c1a2a00fb2c38354b48
ieva-k <ievakaula@gmail.com>
2020-12-26 14:03:39 +0200

    added pages

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <section>
+      <div className="container">
+        <div className="row center-xs">
+          <div className="col-xs-12">
+            <h1>This is Home Page</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Home;
---
+++ src/pages/Post.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Post = () => {
+  return (
+    <section>
+      <div className="container">
+        <div className="row center-xs">
+          <div className="col-xs-12">
+            <h1>This is Post Page</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Post;
