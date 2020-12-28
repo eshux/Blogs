d46ffdaf89de18c2e123b8ce1558a9ca3ab6f6e3
eshux <54838488+eshux@users.noreply.github.com>
2020-12-28 17:21:18 +0200

    create-login/ added login window

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux';
 import { postReducer } from './posts/reducer';
 import { commentReducer } from './comments/reducer';
+import { loginReducer } from './login/reducer';
+
 
 const rootReducer = combineReducers({
   postReducer,
   commentReducer,
+  loginReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
